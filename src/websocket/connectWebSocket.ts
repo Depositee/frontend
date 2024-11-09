9cadc7ed0ef50744c3b71a52267c29d38d67f2e2
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-09 21:16:37 +0700

    [ADD] create auth for each notification client

---
+++ src/websocket/connectWebSocket.ts
@@ -0,0 +1,38 @@
+import { UserData } from "@/interface/auth/user";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+let currentWebSocket: WebSocket | null = null;
+
+export const connectWebSocket = (user: UserData) => {
+    const userId = user.id;
+
+    if (currentWebSocket) {
+        currentWebSocket.close();
+        currentWebSocket = null;
+    }
+
+    const ws = new WebSocket(`ws://localhost:8088?userId=${userId}`);
+    currentWebSocket = ws;
+
+    ws.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        toast.info(`New notification: ${message.message}`);
+    };
+
+    ws.onopen = () => console.log("Connected to WebSocket");
+    ws.onerror = (error) => console.error("WebSocket error:", error);
+    ws.onclose = () => {
+        console.log("WebSocket connection closed");
+        if (currentWebSocket === ws) {
+            currentWebSocket = null;
+        }
+    };
+
+    return () => {
+        if (currentWebSocket) {
+            currentWebSocket.close();
+            currentWebSocket = null;
+        }
+    };
+};
