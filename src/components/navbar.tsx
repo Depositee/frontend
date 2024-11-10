da43a4ff5e4c67db5b8b4f831894b0b81bda8cc9
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-10 23:37:18 +0700

    [FIX] move notification to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Image from "next/image";
 import NavItem from "./navItems";
 import { AuthContext } from "@/contexts/AuthContext";
 import logout from "@/api/auth/logout.api";
+import { UserData } from "@/interface/auth/user";
+import { connectWebSocket } from "@/websocket/connectWebSocket";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Navbar() {
 
-  const { isLogin , setIsLogin , setCurrentUser} = useContext(AuthContext);
+  const { isLogin , setIsLogin ,currentUser, setCurrentUser} = useContext(AuthContext);
 
   const handleLogout = async() =>{
     await logout()
@@ -15,6 +19,13 @@ export default function Navbar() {
     setCurrentUser(null)
     window.location.reload()
   }
+
+  useEffect(() => {
+    if(isLogin && currentUser != null && currentUser.data){
+      const userData : UserData = currentUser.data.user
+      connectWebSocket(userData)
+    }
+  }, [currentUser, isLogin]);
   return (
     <div className="h-16 fixed top-0 left-0 right-0 z-30 flex flex-row justify-end items-center px-8 gap-8">
       {<NavItem path="/" title="Home" />}
@@ -30,6 +41,7 @@ export default function Navbar() {
       <div className="relative h-full justify-center align-center flex">
         <Image src="/globe.svg" alt="logo" width={24} height={24} />
       </div>
+      <ToastContainer />
     </div>
   );
 };
