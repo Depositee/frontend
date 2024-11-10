4c1a15f7bc5b844d607b7a10d57632a1185f52f3
preamza02 <preamza02@gmail.com>
2024-11-11 00:01:30 +0700

    edit config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 config({ path: `.env` });
 
-export const { GATE_WAY_URL } = process.env ?? "localhost";
-export const { GATE_WAY_PORT } = process.env ?? 3000;
-export const { RABBIT_MQ_URL } = process.env ?? "localhost";
+export const GATE_WAY_URL = process.env.GATE_WAY_URL ?? "localhost";
+export const GATE_WAY_PORT = process.env.GATE_WAY_PORT ?? 3000;
+export const RABBIT_MQ_URL = process.env.RABBIT_MQ_URL ?? "localhost";
