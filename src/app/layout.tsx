a7b4797fe7872330aa98e462d4035406b6f330da
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-10 23:54:57 +0700

    [ADD] add loading component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,22 @@ import '@fontsource/geist-mono/700.css';
 import '@fontsource/geist-mono/800.css';
 import '@fontsource/geist-mono/900.css';
 import { AuthProvider } from "@/contexts/AuthContext";
+import { ErrorProvider } from "@/contexts/ErrorContext";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Depositee",
   description: "",
 };
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+      <span className="ml-4 text-blue-500 font-medium">Loading...</span>
+    </div>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -27,10 +38,16 @@ export default function RootLayout({
       <body
         className={`font-sans`}
       >
+        <ErrorProvider>
         <AuthProvider>
-          <Navbar/>
-          {children}
-        </AuthProvider>
+            <Suspense fallback={<Loading />}>
+              <Navbar />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
+              {children}
+            </Suspense>
+          </AuthProvider>
+        </ErrorProvider>
       </body>
     </html>
   );
