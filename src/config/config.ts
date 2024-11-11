8999544943fc0e0fdc96f5d7cd55aaabacba3d08
preamza02 <preamza02@gmail.com>
2024-11-11 15:22:47 +0700

    edit apis

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ config();
 
 console.log(process.env, "process.env");
 export const GATE_WAY_URL =
-  process.env.NEXT_PUBLIC_GATE_WAY_URL ?? "localhost/api";
+  process.env.NEXT_PUBLIC_GATE_WAY_URL ?? "localhost/apis";
 // export const GATE_WAY_PORT = process.env.NEXT_PUBLIC_GATE_WAY_PORT ?? 3000;
 export const RABBIT_MQ_URL =
   process.env.NEXT_PUBLIC_RABBIT_MQ_URL ?? "localhost";
