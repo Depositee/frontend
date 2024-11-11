d96e067d41411dc222c651895e462b086f88d41c
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-12 00:42:29 +0700

    [Update] fetch user data in updateOrder form

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -7,24 +7,64 @@ import { useParams } from "next/navigation";
 import updateOrderById from "@/api/order/updateOrder.api";
 import Loading from "@/components/loading";
 import { ErrorContext} from "@/contexts/ErrorContext";
+import getUserData from "@/api/auth/getUserData.api";
+import { GetUserData } from "@/interface/auth/user";
+import ReviewPopup from "../components/ReviewPopup";
+import Review from "../components/Review";
 
 export default function UpdateOrderPage() {
   const { id } = useParams<{ id: string }>();
   const [orderData, setOrderData] = useState<Order | null>(null);
+  const [depositorUsername, setDepositorUsername] = useState<string | null>(null);
+  const [depositeeUsername, setDepositeeUsername] = useState<string | null>(null);
+  const onCloseModal = () => setShowPopup(false);
+  const [showPopup, setShowPopup] = useState(false);
+
   const {setError} = useContext(ErrorContext)
 
   useEffect(() => {
-    const fetchOrder = async () => {
-      const order = await getOrderById(id);
-      if (order.success && order.data.data) {
-        setOrderData(order.data.data);
-      } else {
-        setError(order.error);
-      }
-    };
-
-    fetchOrder();
+    fetchOrder()
   }, [id]);
+
+  useEffect(() => {
+    if (orderData) {
+      getDepositorUsername();
+      getDepositeeUsername();
+    }
+  }, [orderData]);
+
+  
+  const fetchOrder = async () => {
+    const order = await getOrderById(id);
+    if (order.success && order.data.data) {
+      setOrderData(order.data.data);
+    } else {
+      setError(order.error);
+    }
+  };
+
+  const getDepositorUsername = async() =>{
+    if(orderData){
+      const depositorData : GetUserData | undefined= await getUserData(orderData.depositor_id)
+      if(depositorData){
+        setDepositorUsername(depositorData.data.username)
+      }
+    }
+  }
+
+  const getDepositeeUsername = async() =>{
+    if(orderData && orderData.depositee_id){
+      const depositeeData : GetUserData | undefined= await getUserData(orderData.depositee_id)
+      if(depositeeData){
+        setDepositeeUsername(depositeeData.data.username)
+      }
+    }
+  }
+
+  const handleReviewButton = async(e : React.FormEvent) =>{
+    e.preventDefault()
+    setShowPopup(true)
+  }
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -106,14 +146,13 @@ export default function UpdateOrderPage() {
     <div className="w-full flex justify-center bg-amber-100 text-stone-900/80 h-screen p-4 pb-40 md:pb-20">
       <div className="flex w-full h-full p-5 pt-12">
         <Sidebar />
-
         <div className="flex flex-col w-full max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg overflow-y-scroll">
           {orderData ? (
             <>
               <h2 className="text-2xl font-semibold mb-4">
                 {orderData.package_name}
               </h2>
-              <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="space-y-4">
                 {/* Package Description */}
                 <div>
                   <label
@@ -188,35 +227,47 @@ export default function UpdateOrderPage() {
                     htmlFor="depositor_id"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Depositor ID
+                    Depositor
                   </label>
                   <input
                     id="depositor_id"
                     type="text"
-                    value={orderData.depositor_id}
+                    value={depositorUsername ?? 'N/A'}
                     className="w-full p-2 mt-1 border rounded-md bg-gray-100 text-gray-600"
                     disabled
                   />
                 </div>
 
-                {/* Depositee ID */}
+                {/* Depositee ID with Review Button */}
                 {orderData.depositee_id ? (
                   <div>
                     <label
                       htmlFor="depositee_id"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Depositee ID
+                      Depositee
                     </label>
-                    <input
-                      id="depositee_id"
-                      type="text"
-                      value={orderData.depositee_id}
-                      className="w-full p-2 mt-1 border rounded-md bg-gray-100 text-gray-600"
-                      disabled
-                    />
+                    <div className="flex items-center space-x-2 mt-1">
+                      <input
+                        id="depositee_id"
+                        type="text"
+                        value={depositeeUsername ?? 'N/A'}
+                        className="w-full p-2 border rounded-md bg-gray-100 text-gray-600"
+                        disabled
+                      />
+                     {orderData.status && orderData.status === 'completed'? 
+                      <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition duration-200"
+                        onClick={handleReviewButton}
+                      >
+                        Review
+                      </button>
+                      : null
+                     }
+                    </div>
                   </div>
                 ) : null}
+
                 {/* Status */}
                 <div>
                   <label
@@ -242,7 +293,12 @@ export default function UpdateOrderPage() {
                     {getUpdateButtonText()}
                   </button>
                 ) : null}
-              </form>
+              </div>
+              {showPopup && (
+              <ReviewPopup onClose={onCloseModal}>
+                <Review onClose={onCloseModal}/>
+              </ReviewPopup>
+        )}
             </>
           ) : (
             <Loading/>
